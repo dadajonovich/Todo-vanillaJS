75d0a76ada8ab0834069229ff3ce80a9d2e94547
Norbaev <timnrbv@gmail.com>
2023-03-04 18:14:20 +0500

    rewrote the code to fit the functional paradigm

--- app.js
+++ app.js
@@ -1,136 +1,181 @@
 'use strict';
 
-(() => {
-  // Elements
-  const objOfTasks = JSON.parse(localStorage.getItem('tasks')) || {};
-  const listContainer = document.querySelector('.list');
-  const form = document.forms['addTask'];
-  const input = form.elements['taskText'];
-  const counter = document.querySelector('.list__counter');
-  const name = document.querySelector('.footer');
-  const inputText = document.querySelector('.input__text');
+let objOfTasks = JSON.parse(localStorage.getItem('tasks')) || {};
+const listContainer = document.querySelector('.list');
+const form = document.forms['addTask'];
+const input = form.elements['taskText'];
+const counter = document.querySelector('.list__counter');
+const name = document.querySelector('.footer');
+const inputText = document.querySelector('.input__text');
 
-  form.addEventListener('submit', onFormSubmitHandler);
-  listContainer.addEventListener('click', onDeleteHandler);
-  name.addEventListener('mouseover', renameHandler);
-  name.addEventListener('mouseout', renameHandler);
-  window.addEventListener('resize', (e) => {
-    if (document.documentElement.clientWidth < 600) {
-      inputText.placeholder = '???';
-    } else inputText.placeholder = 'Что делать будем?';
-  });
+window.addEventListener('resize', (e) => {
+  if (document.documentElement.clientWidth < 600) {
+    inputText.placeholder = '???';
+  } else inputText.placeholder = 'Что делать будем?';
+});
 
-  function renderAllTasks(tasks, fn) {
-    try {
-      if (!tasks) {
-        throw new Error('Нет задач!');
-      }
-
-      const fragment = document.createDocumentFragment();
-      Object.values(tasks).forEach((task) => {
-        const li = fn(task);
-        fragment.append(li);
-      });
-
-      listContainer.append(fragment);
-    } catch (error) {
-      console.log(error.message);
+const renderAllTasks = (tasks, fnListItemTemplate) => {
+  try {
+    if (!tasks) {
+      throw new Error('Нет задач!');
     }
+
+    const fragment = document.createDocumentFragment();
+    Object.values(tasks).forEach((task) => {
+      const li = fnListItemTemplate(task);
+      fragment.append(li);
+      return true;
+    });
+
+    listContainer.append(fragment);
+  } catch (error) {
+    console.log(error.message);
+    return error;
   }
+};
 
-  function listItemTemplate({ _id, body } = {}) {
-    const li = document.createElement('li');
-    li.classList.add('list__item');
-    li.setAttribute('data-task-id', _id);
+const listItemTemplate = ({ _id, body } = {}) => {
+  const li = document.createElement('li');
+  li.classList.add('list__item');
+  li.setAttribute('data-task-id', _id);
 
-    const wrapper = document.createElement('div');
-    wrapper.classList.add('list__task');
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('list__task');
 
-    const article = document.createElement('p');
-    article.classList.add('list__text');
-    article.textContent = body;
+  const article = document.createElement('p');
+  article.classList.add('list__text');
+  article.textContent = body;
 
-    const deleteBtn = document.createElement('span');
-    deleteBtn.classList.add('list__delete-btn');
-    deleteBtn.insertAdjacentHTML('afterbegin', '&#10006;');
+  const deleteBtn = document.createElement('span');
+  deleteBtn.classList.add('list__delete-btn');
+  deleteBtn.insertAdjacentHTML('afterbegin', '&#10006;');
 
-    wrapper.append(article, deleteBtn);
+  wrapper.append(article, deleteBtn);
 
-    const vl = document.createElement('div');
-    vl.classList.add('list__vl');
+  const vl = document.createElement('div');
+  vl.classList.add('list__vl');
 
-    li.append(wrapper, vl);
+  li.append(wrapper, vl);
 
-    return li;
-  }
+  return li;
+};
 
-  renderAllTasks(objOfTasks, listItemTemplate);
+renderAllTasks(objOfTasks, listItemTemplate);
 
-  function onFormSubmitHandler(event) {
-    event.preventDefault();
-    const value = input.value;
-
+const getInput = (inputFromDom) => {
+  try {
+    const value = inputFromDom.value;
     if (!value) {
-      // alert('Нет дел - нет проблем!');
-      Swal.fire('Нет дел - нет проблем!');
-      return;
+      throw Error;
     }
-    const task = createNewTask(value);
-    const listItem = listItemTemplate(task);
-    listContainer.prepend(listItem);
     form.reset();
+    return value;
+  } catch (error) {
+    Swal.fire('Нет дел - нет проблем!');
+    throw error('Пустой input - код остановлен!');
   }
+};
 
-  function createNewTask(body) {
-    const newTask = {
-      body,
-      completed: false,
-      _id: `task-${Math.random()}`,
-    };
-    objOfTasks[newTask._id] = newTask;
-    changeLocalStorage(objOfTasks);
-    return { ...newTask };
-  }
+const createNewTask = (fnListItemTemplate) => (body) => {
+  const newTask = {
+    body,
+    completed: false,
+    _id: `task-${Math.random()}`,
+  };
+  const listItem = fnListItemTemplate(newTask);
+  listContainer.prepend(listItem);
+  return newTask;
+};
 
-  function deleteTask(id) {
-    const isConfirm = confirm('С глаз долой?');
-    if (!isConfirm) return isConfirm;
-    delete objOfTasks[id];
-    changeLocalStorage(objOfTasks);
-    return isConfirm;
-  }
+const curryCreateNewTask = createNewTask(listItemTemplate);
 
-  function deleteTaskFromHtml(el, confirmed) {
-    if (!confirmed) return;
-    el.remove();
-  }
+const addTaskToObj = (task) => {
+  const newObjWithTask = {
+    ...objOfTasks,
+    [task._id]: task,
+  };
+  objOfTasks = { ...newObjWithTask };
+  return newObjWithTask;
+};
 
-  function onDeleteHandler({ target }) {
-    if (target.classList.contains('list__delete-btn')) {
-      const parent = target.closest('[data-task-id]');
-      const id = parent.dataset.taskId;
-      const confirmed = deleteTask(id);
-      deleteTaskFromHtml(parent, confirmed);
-    } else return;
-  }
-
-  function renameHandler(event) {
-    const type = event.type;
-    if (type === 'mouseover') {
-      name.textContent = 'Привет, Карен!';
-    } else name.textContent = '© Made By Timur Norbaev 2023';
-  }
-
-  function changeLocalStorage(objOfTasks) {
-    const tasksOnJson = JSON.stringify(objOfTasks);
+function changeLocalStorage(obj) {
+  try {
+    const tasksOnJson = JSON.stringify(obj);
     localStorage.setItem('tasks', tasksOnJson);
-    countListItems(objOfTasks);
+    return obj;
+  } catch (error) {
+    throw error('Произошла ошибка в changeLocalStorage');
   }
+}
 
-  function countListItems(tasks) {
-    const lenghtObj = Object.keys(tasks).length;
-    if (lenghtObj != 0) {
-      counter.textContent = `${lenghtObj} items`;
-    } else counter.textContent = 'Безделье это игрушка дьявола...';
+const countListItems = (tasks) => {
+  const lenghtObj = Object.keys(tasks).length;
+  if (lenghtObj != 0) {
+    counter.textContent = `${lenghtObj} items`;
+  } else counter.textContent = 'Безделье это игрушка дьявола...';
+};
+
+document.addEventListener('DOMContentLoaded', (e) =>
+  countListItems(objOfTasks)
+);
+
+const compose =
+  (...fns) =>
+  (arg) =>
+    fns.reduce((composed, f) => f(composed), arg);
+
+const addingCycle = compose(
+  getInput,
+  curryCreateNewTask,
+  addTaskToObj,
+  changeLocalStorage,
+  countListItems
+);
+
+const onFormSubmitHandler = (e) => {
+  e.preventDefault();
+  return addingCycle(input);
+};
+
+form.addEventListener('submit', (e) => onFormSubmitHandler(e));
+
+// Delete
+
+const getIdElement = ({ target }) => {
+  try {
+    if (!target.classList.contains('list__delete-btn')) throw Error;
+    const parent = target.closest('[data-task-id]');
+    const id = parent.dataset.taskId;
+    console.log(id);
+    // const confirmed = deleteTask(id);
+    // deleteTaskFromHtml(parent, confirmed);
+    return { id, parent };
+  } catch (error) {
+    throw error('Событие не на кнопке удаления!');
   }
-})();
+};
+
+const delTask = ({ id, parent }) => {
+  const isConfirm = confirm('С глаз долой?');
+  if (!isConfirm) return isConfirm;
+  const newObjWithTask = {
+    ...objOfTasks,
+  };
+  delete newObjWithTask[id];
+  parent.remove();
+  objOfTasks = { ...newObjWithTask };
+
+  return newObjWithTask;
+};
+
+const deleteCycl = compose(
+  getIdElement,
+  delTask,
+  changeLocalStorage,
+  countListItems
+);
+
+const onDeleteHandler = (e) => {
+  return deleteCycl(e);
+};
+listContainer.addEventListener('click', (e) => onDeleteHandler(e));
